a5692a350ed3798f13c284a1357321b86198ce0f
KassmanBen <kassmanben@gmail.com>
2018-09-23 20:27:55 -0700

    storage test 1

--- main.js
+++ main.js
@@ -27788,35 +27788,7 @@ class App extends __WEBPACK_IMPORTED_MODULE_0_react__["Component"] {
     window.addEventListener('resize', this.handleResize.bind(this));
     this.setState({ isLoading: false });
     const epsiodesRef = __WEBPACK_IMPORTED_MODULE_6__firebase__["a" /* default */].database().ref('eps');
-    const storage = __WEBPACK_IMPORTED_MODULE_6__firebase__["a" /* default */].storage();
-    var testRef = storage.child('test/Screen Shot 2018-09-22 at 4.40.16 PM.png');
 
-    // Get the download URL
-    testRef.getDownloadURL().then(function (url) {
-      var img = document.getElementById('myimg');
-      img.src = url;
-    }).catch(function (error) {
-
-      // A full list of error codes is available at
-      // https://firebase.google.com/docs/storage/web/handle-errors
-      switch (error.code) {
-        case 'storage/object_not_found':
-          // File doesn't exist
-          break;
-
-        case 'storage/unauthorized':
-          // User doesn't have permission to access the object
-          break;
-
-        case 'storage/canceled':
-          // User canceled the upload
-          break;
-
-        case 'storage/unknown':
-          // Unknown error occurred, inspect the server response
-          break;
-      }
-    });
     epsiodesRef.on('value', snapshot => {
       let episodes = snapshot.val();
       let newState = [];
