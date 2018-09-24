0dd8cb2bab62867c2218ea74addb48c62f17ee56
KassmanBen <kassmanben@gmail.com>
2018-09-23 19:47:26 -0700

    storage test

--- main.js
+++ main.js
@@ -27789,25 +27789,33 @@ class App extends __WEBPACK_IMPORTED_MODULE_0_react__["Component"] {
     this.setState({ isLoading: false });
     const epsiodesRef = __WEBPACK_IMPORTED_MODULE_6__firebase__["a" /* default */].database().ref('eps');
     const storage = __WEBPACK_IMPORTED_MODULE_6__firebase__["a" /* default */].storage();
-    var storageRef = storage.refFromURL("gs://podscript-214820.appspot.com/test/Screen Shot 2018-09-22 at 4.40.16 PM.png");
-    console.log(storageRef);
-    storageRef.child('test/Screen Shot 2018-09-22 at 4.40.16 PM.png').getDownloadURL().then(function (url) {
-      // `url` is the download URL for 'images/stars.jpg'
+    var testRef = storageRef.child('test/Screen Shot 2018-09-22 at 4.40.16 PM.png');
 
-      // This can be downloaded directly:
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'blob';
-      xhr.onload = function (event) {
-        var blob = xhr.response;
-      };
-      xhr.open('GET', url);
-      xhr.send();
-
-      // Or inserted into an <img> element:
+    // Get the download URL
+    testRef.getDownloadURL().then(function (url) {
       var img = document.getElementById('myimg');
       img.src = url;
     }).catch(function (error) {
-      // Handle any errors
+
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case 'storage/object_not_found':
+          // File doesn't exist
+          break;
+
+        case 'storage/unauthorized':
+          // User doesn't have permission to access the object
+          break;
+
+        case 'storage/canceled':
+          // User canceled the upload
+          break;
+
+        case 'storage/unknown':
+          // Unknown error occurred, inspect the server response
+          break;
+      }
     });
     epsiodesRef.on('value', snapshot => {
       let episodes = snapshot.val();
